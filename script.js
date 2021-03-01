8c45bccd12222644d175fff3222688380dac1cb5
mralimov <mralimov89@gmail.com>
2021-03-01 13:35:42 -0500

    Stiky Nav Intersection observer API

--- script.js
+++ script.js
@@ -128,11 +128,21 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 const initialCoords = section1.getBoundingClientRect();
 
 ////////////////////////////////////
-//Sticky NAV
-window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
+//Sticky Nav modern way: Intersection Observer API
 
-  if (window.scrollY > initialCoords.top) {
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = entries => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
     nav.classList.add('sticky');
   } else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
